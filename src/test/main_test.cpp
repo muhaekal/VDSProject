94120a5339ec7e03f96a36b99774e46f5d7c2ce9
Haekal Muhammad <mdhaekal@gmail.com>
2023-12-10 02:33:52 +0100

    testing main_test.cpp

--- src/test/main_test.cpp
+++ src/test/main_test.cpp
@@ -5,6 +5,57 @@
 
 #include "Tests.h"
 
+// Define a test fixture for the Manager class
+class ManagerTest : public ::testing::Test {
+protected:
+    ClassProject::Manager manager;  // Create an instance of Manager class
+
+    // This function will be called before each test
+    void SetUp() override {
+        // Initialize any test-specific resources
+    }
+
+    // This function will be called after each test
+    void TearDown() override {
+        // Clean up any test-specific resources
+    }
+
+    // You can declare additional member functions or variables here
+};
+
+// Define a test case for the createNode function
+TEST_F(ManagerTest, CreateNodeTest) {
+
+
+    ClassProject::BDD_ID ID_a = manager.createNode(0, 1, 2, "a");
+ 
+    EXPECT_EQ(manager.uniqueTableSize(), 3);
+    EXPECT_EQ(manager.uniqueTableMap.size(), 3);
+    EXPECT_EQ(ID_a, 2);
+    
+}
+
+TEST_F(ManagerTest, CreateVarTest) {
+    
+
+    ClassProject::BDD_ID ID_a = manager.createVar("a");
+    ClassProject::BDD_ID ID_b = manager.createVar("b");
+ 
+    EXPECT_EQ(ID_a, 2);
+    EXPECT_EQ(ID_b, 3);
+    
+}
+
+TEST_F(ManagerTest, keyGenTest) {
+   
+
+    size_t d = manager.keyGen(1,1,1);
+ 
+    EXPECT_EQ(d, 4398048608257);
+    //EXPECT_EQ(ID_b, 3);
+    
+}
+
 int main(int argc, char* argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);
